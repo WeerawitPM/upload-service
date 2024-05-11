6fe9197722b95023158ba261ffa8427b5947e7dd
Weerawit Phumphuang <weerawit.pm@gmail.com>
2024-05-11 11:35:50 +0700

    update: api delete

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var app = express(); //set express ไว้เป็นตัวแปร app
 const multer = require('multer');
 const port = 3001; //set ตัวแปร port เท่ากับ 3000
 const cors = require('cors'); // import cors module
+const fs = require('fs');
 
+app.use(express.json());
 app.use(cors());
+
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 //ใช้ get เพื่อเรียกไฟล์ index.html
@@ -13,18 +16,12 @@ app.get('/', (req, res) => {
 })
 
 //ใช้ post เพื่อรองรับการ upload
-app.post('/upload', (req, res) => {
+app.post('/upload/userProfile', (req, res) => {
     const storage = multer.diskStorage({
         destination: function (req, file, callback) {
             callback(null, __dirname + '/uploads/userProfiles') // folder ที่เราต้องการเก็บไฟล์
         },
         filename: function (req, file, callback) {
-            // ตรวจสอบชนิดของไฟล์ก่อนที่จะบันทึก
-            if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-                // ถ้าไฟล์ไม่ได้เป็นภาพ ก็สร้าง error และไม่บันทึกไฟล์
-                return callback(new Error('Only image files are allowed!'), false);
-            }
-
             const username = req.body.username; // ดึงค่า username จากข้อมูลที่ส่งมา
             const originalName = file.originalname; // ชื่อเดิมของไฟล์
             const fileExtension = originalName.split('.').pop(); // นามสกุลของไฟล์
@@ -44,12 +41,39 @@ app.post('/upload', (req, res) => {
             return res.status(500).send(err);
         }
 
-        const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}`;
+        const imageUrl = `${req.protocol}://${req.get('host')}/uploads/userProfiles/${req.file.filename}`;
 
         // ส่ง URL ของไฟล์ภาพกลับไปยังเว็บไซต์ผ่าน API
         res.json({ imageUrl: imageUrl });
     });
 })
+
+app.delete('/delete/userProfile', (req, res) => {
+    const image = req.body.image; // เก็บ URL ของรูปภาพที่จะลบ
+
+    // ตรวจสอบว่า URL ถูกส่งมาหรือไม่
+    if (!image) {
+        return res.status(400).send("Missing image URL");
+    }
+
+    // ดึงชื่อไฟล์จาก URL ด้วยการแยกส่วนที่มีชื่อไฟล์ตั้งแต่ /uploads/userProfiles/ ไปจนถึงสิ้นสุด
+    const fileName = image.split('/').pop();
+
+    // เส้นทางไฟล์ที่ต้องการลบ
+    const filePath = __dirname + '/uploads/userProfiles/' + fileName;
+
+    // ลบไฟล์โดยใช้ fs.unlink
+    fs.unlink(filePath, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Failed to delete image");
+        }
+        
+        res.status(200).send("Image deleted successfully");
+    });
+});
+
+
 
 // ใช้ listen เพื่อระบุว่า website จะทำงานที่ port อะไร เราใช้ให้เรียกตัวแปร port
 app.listen(port, () => {
